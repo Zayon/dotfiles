7b7300a2862d8e1ffd69e92d897a8fcd83db5a7f
Godinez Pablo <pablo.godinez@zayon.eu>
2019-08-03 18:09:11 +0200

    Update fish config

--- fish/.config/fish/conf.d/functions/wallpaper.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function wallpaper
-	~/.config/i3/setWallpaper.sh $argv
-end
--- fish/.config/fish/functions/composer.fish
+++ fish/.config/fish/functions/composer.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function composer --description 'alias composer=docker run -it --rm -u (id -u):(id -g) -v (echo "$PWD"):/app -v /tmp:/tmp composer composer'
-	docker run -it --rm -u (id -u):(id -g) -v (echo "$PWD"):/app -v /tmp:/tmp composer composer $argv;
+function composer --description 'alias composer=docker run --rm -it -v "$PWD":/app -v /tmp:/tmp -u (id -u):(id -g) composer composer'
+	docker run --rm -it -v "$PWD":/app -v /tmp:/tmp -u (id -u):(id -g) composer composer $argv;
 end
