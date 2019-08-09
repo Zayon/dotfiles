c588ac4224aa9aad1b648065bde95177ce993f7e
Godinez Pablo <pablo.godinez@zayon.eu>
2019-08-09 17:16:01 +0200

    Bump polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,24 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  screens=$(xrandr --query | grep " connected" | cut -d" " -f1)
-  nbScreen=$(wc -l <<<"$(echo "$screens")")
+screens=$(xrandr --query | grep " connected" | cut -d" " -f1)
 
-  if [[ nbScreen == 1 ]]; then
-    TRAY_POS=right polybar example &
-  else
-    primary=$(xrandr --query | grep primary | cut -d" " -f1)
-
-    for m in $screens; do
-      if [[ $primary == $m ]]; then
-          traypos="right"
-      else
-          traypos="none"
-      fi
-      MONITOR=$m TRAY_POS=$traypos polybar example &
-    done
-  fi
-else
+if [[ $(wc -l <<<"$(echo "$screens")") == 1 ]]; then
   TRAY_POS=right polybar example &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [[ $primary == $m ]]; then
+        traypos="right"
+    else
+        traypos="none"
+    fi
+    MONITOR=$m TRAY_POS=$traypos polybar example &
+  done
 fi
