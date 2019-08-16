b6747293cb6a542e89c536d7a3a52169af367a4b
Godinez Pablo <pablo.godinez@evaneos.com>
2019-08-16 12:11:58 +0200

    Bump

--- fish/.config/fish/functions/php.fish
+++ fish/.config/fish/functions/php.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function php --description 'alias php=docker run --rm -it -v "$PWD":/app -u (id -u):(id -g) php:cli php'
-	docker run --rm -it -v "$PWD":/app -u (id -u):(id -g) php:cli php $argv;
+function php --description 'alias php=docker run --rm -it -v "$PWD":/app -w /app -u (id -u):(id -g) php:cli php'
+	docker run --rm -it -v "$PWD":/app -w /app -u (id -u):(id -g) php:cli php $argv;
 end
