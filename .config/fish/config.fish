e134d662c110919b3ed582b47d1fe00cb9865ad6
Godinez Pablo <pablo.godinez@zayon.eu>
2020-09-03 15:00:08 +0200

    bump

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,8 @@ set -x TERM xterm-256color
 
 set -g theme_date_timezone Europe/Paris
 set -g theme_date_format "+%a %d %b %Y %T"
+set -g theme_display_k8s_namespace yes
 
-set -g theme_vcs_ignore_paths "$HOME" # Needs change in __bobthefish_ignore_vcs_dir
+# set -g theme_vcs_ignore_paths "$HOME" # Needs change in __bobthefish_ignore_vcs_dir
 
 source ~/.profile
